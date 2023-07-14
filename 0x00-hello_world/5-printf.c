4456c3198a20e9484d9f6493ff22637f9f15321a
ehisd <omoanondan@gmail.com>
2023-07-14 06:28:27 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Alwaysys 0 (Success)
+ */
+ int main(void)
+{
+	print("with proper grammar, but the outcome is a place of art");
+	return (0);
+}
