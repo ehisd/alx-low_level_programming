84f6875475d11f435223b59158c3cac66c2899e3
ehisd <omoanondan@gmail.com>
2023-07-21 21:22:56 -0700

    mh gd

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * _mul - check if a character is a digit
+ * mul - check if a character is a digit
  * @x: The number to be multiply
  * @y: The second number to be multiply with y
  * Return: The result of multiplication between x and y para
  */
 
-int _mul(int x, int y)
+int mul(int x, int y)
 {
-	return (x = y);
+	return (x * y);
 }
